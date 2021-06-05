18fc2c87df11db7ae14f8ead2d1d99c25ac8f01e
Braden Betz <bradenb@hawaii.edu>
2021-06-04 20:50:05 -1000

    Added Consensus Algorithm and Conflict Resolution
    Added the Consensus algorithm that checks to make sure a given copy of the blockchain is the correct one, and a conflict resolution function that resolves any conflicts with a given blockchain.

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
 from time import time
+from urllib.parse import urlparse
+import requests
 
 """
    This project was done to learn how a Blockchain functions on a technical level, to better understand how they work
@@ -14,9 +16,82 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
 
         # Create the Genesis(starting) block here
         self.new_block(previous_hash=1, proof=100)
+
+    def register_node(self, address):
+        """
+        Add a new node(miner/computer) to the list of nodes
+        :param address: Address of the node
+        :return: None
+        """
+
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain):
+        """
+        Determine if the given blockchain is valid (Consensus Algorithm)
+        :param chain: The blockchain to verify
+        :return: <bool> True if valid, false if not
+        """
+
+        last_block = chain[0]
+        current_index = 1
+        # Can probably optimize this later
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof-Of-Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        """
+        This is the consensus algorithm. It resolves conflicts with a given chain by replacing it by the longest one in
+        the network.
+        :return: <bool> True if the chain was replaced, False if not
+        """
+
+        neighbors = self.nodes
+        new_chain = None
+
+        # We are only looking for chains longer than the one our node currently holds
+
+        max_length = len(self.chain)
+
+        # Grab and verify the chains from all the nodes in our network
+
+        for node in neighbors:
+            response = requests.get(f'https://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if the length is longer and if the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if we discovered a new valid chain longer than the one our node holds
+        if new_chain:
+            self.chain = new_chain
+            return True
+        return False
 
     def new_block(self, proof, previous_hash=None):
         """
@@ -96,40 +171,3 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
-
-"""
-# Create our node that will validate transactions and add blocks to the blockchain
-app = Flask(__name__)
-
-# Create a unique address for this node
-
-node_identifier = str(uuid4()).replace('-', '')
-
-# instiantiate the Blockchain
-
-blockchain = Blockchain()
-
-
-@app.route('/mine', methods=['GET'])
-def mine():
-    return "We'll mine a new block"
-
-
-@app.route('/transactions/new', methods=['POST'])
-def new_transaction():
-    return "We'll add a new Transaction"
-
-
-@app.route('/chain', methods=['GET'])
-def full_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
-    }
-
-    return jsonify(response), 200
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000)
-"""
